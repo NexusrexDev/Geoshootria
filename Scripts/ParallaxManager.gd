4ccd593ae2fd38b342711c04956311e5e246acf4
Nexusrex <ahmedtahoun55@gmail.com>
2024-01-27 03:52:12 +0200

    Change: Updated background visuals. Separated the background into a new node, changed the inner structure, updated to new artwork.

--- Scripts/ParallaxManager.gd
+++ Scripts/ParallaxManager.gd
@@ -3,10 +3,9 @@ extends ParallaxBackground
 
 # Variables
 export var speed = 10
-onready var layers = [$FarTree,$CloseTree,$Bush,$Ground]
+onready var layers = [$FarTree,$CloseTree,$Ground]
 
 func _process(delta):
-	layers[0].motion_offset.x -= 2 * speed * delta
-	layers[1].motion_offset.x -= 4.5 * speed * delta
-	layers[2].motion_offset.x -= 6 * speed * delta
-	layers[3].motion_offset.x -= 6 * speed * delta
+	layers[0].motion_offset.x -= 2 * speed * delta #Back Tree
+	layers[1].motion_offset.x -= 4.5 * speed * delta #Front Tree
+	layers[2].motion_offset.x -= 6 * speed * delta #Ground
